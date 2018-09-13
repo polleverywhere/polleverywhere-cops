03e25d077578b09d35598f07a9dd08577be20c5b
Mike Foley <foley3@gmail.com>
2018-09-13 15:21:48 -0700

    Minor copy changes to RenderViews message text

--- lib/rubocop/cop/polleverywhere/render_views.rb
+++ lib/rubocop/cop/polleverywhere/render_views.rb
@@ -4,7 +4,7 @@ module RuboCop
   module Cop
     module PollEverywhere
       class RenderViews < Cop
-        MSG = 'Do not use `render_views` in controller tests because it is an order of magnitude slower. If view rendering needs to be tested, write a different type of test.'.freeze
+        MSG = 'Do not use `render_views` in controller specs because it is orders of magnitude slower. If view rendering needs to be tested, write a different type of spec (acceptance, view, etc.)'.freeze
 
         def_node_matcher :render_views?, <<-PATTERN
           (send nil? :render_views)
